90b5571cf8d39903dac922954627b4183b6982cf
hasnain7860 <httechnical7860@gmail.com>
2024-08-05 07:41:45 +0500

    week 8

--- videoDocuments.js
+++ videoDocuments.js
@@ -1,5 +1,131 @@
 const videoDocuments = [
   {
+    "url": "https://drive.google.com/uc?export=download&id=1eJ-2MRu8wFbzcdNmZKIz0BliTIwBIKJw",
+    "dateTime": "7-8-2024 1:00 AM",
+    "fileName": "Class 6&7 week 8 day 3"
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 6&7 week 8 day 3",
+    "dateTime": "7-8-2024 1:01 AM"
+  },
+  {
+    "url": "https://drive.google.com/uc?export=download&id=1QQsKhMsoFydqJ6_ZcozOpkx-44K0EnPr",
+    "dateTime": "8-8-2024 1:00 AM",
+    "fileName": "Class 6&7 week 8 day 4 part 1 "
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 6&7 week 8 day 4 part 1 ",
+    "dateTime": "8-8-2024 1:01 AM"
+  },
+  {
+    "url": "https://drive.google.com/uc?export=download&id=1qLf_TJDLW784XegE7Pmtx1vF8zxDHACU",
+    "dateTime": "8-8-2024 1:02 AM",
+    "fileName": "Class 6&7 week 8 day 4 part 2"
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 6&7 week 8 day 4 part 2",
+    "dateTime": "8-8-2024 1:03 AM"
+  },
+  {
+    "url": "https://drive.google.com/uc?export=download&id=1hFm8onyqwwfvZ9YIdIoP9izaO_8Us52I",
+    "dateTime": "9-8-2024 1:00 AM",
+    "fileName": "Class 6&7 week 8 day 5"
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 6&7 week 8 day 5",
+    "dateTime": "9-8-2024 1:01 AM"
+  },
+  {
+    "url": "https://drive.google.com/uc?export=download&id=1G1S4bTYrUzgG3OIJ9dcgEqppy6G5eLlJ",
+    "dateTime": "10-8-2024 1:00 AM",
+    "fileName": "Class 6&7 week 8 day 6"
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 6&7 week 8 day 6",
+    "dateTime": "10-8-2024 1:01 AM"
+  },
+  {
+    "url": "https://drive.google.com/uc?export=download&id=1MW35n5FjYyCT6QjAD71FZLlg0ERybQPb",
+    "dateTime": "7-8-2024 1:03 AM",
+    "fileName": "Class 6&7 week 8 day 3 pdf"
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 6&7 week 8 day 3 pdf",
+    "dateTime": "7-8-2024 1:04 AM"
+  },
+  {
+    "url": "https://drive.google.com/file/d/11fFafvojvBxlV0-jMl0QRemX-0GxlJIt/view?usp=drivesdk",
+    "dateTime": "7-8-2024 1:30 AM",
+    "fileName": "Class 8&9 week 8 day 3 project Feature Comparison Table"
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 8&9 week 8 day 3 project Feature Comparison Table",
+    "dateTime": "7-8-2024 1:31 AM"
+  },
+  {
+    "url": "https://drive.google.com/uc?export=download&id=1Zq2NkT-2Gm1F54TflHB8akcwij4onkwN",
+    "dateTime": "8-8-2024 1:30 AM",
+    "fileName": "Class 8&9 week 8 day 4 Music Playlist Layout project "
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 8&9 week 8 day 4 Music Playlist Layout project ",
+    "dateTime": "8-8-2024 1:31 AM"
+  },
+  {
+    "url": "https://drive.google.com/uc?export=download&id=1qebS3J-mOLXEkh7oVNVXntBn-DKkkNr6",
+    "dateTime": "9-8-2024 1:30 AM",
+    "fileName": "Class 8&9 week 8 day 5 portfolio website project part 1"
+  },
+  {
+    "url": "https://drive.google.com/uc?export=download&id=1cE5lA-22nH54-qOxpHKRtMDxPYYHdsFO",
+    "dateTime": "9-8-2024 1:31 AM",
+    "fileName": "Class 8&9 week 8 day 5 portfolio website project part 2"
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 8&9 week 8 day 5 portfolio website",
+    "dateTime": "9-8-2024 1:32 AM"
+  },
+  {
+    "url": "https://drive.google.com/uc?export=download&id=1PGUgs274L69e92r7NlvaXfKxhwFpIMyd",
+    "dateTime": "10-8-2024 1:30 AM",
+    "fileName": "Class 8&9 dashboard layout project "
+  },
+  {
+    "type": "text",
+    "url": "",
+    "fileName": "text",
+    "message": "Class 8&9 dashboard layout project ",
+    "dateTime": "10-8-2024 1:31 AM"
+  }
+,
+  {
     "type": "text",
     "message": "class 8 & 9 photo gallery website project",
     "dateTime": "6-8-2024 1:06 AM"
