7c5704e0874f38f95a8ced70bde16e8fbd927e6f
hasnain7860 <httechnical7860@gmail.com>
2024-08-05 06:39:09 +0500

    week 8 2 day link

--- videoDocuments.js
+++ videoDocuments.js
@@ -1,169 +1,61 @@
 const videoDocuments = [
+[
   {
     "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 8&9 week 7 day 6 practice set pdf ",
-    "dateTime": "3-8-2024 1:03 AM"
+    "message": "class 8 & 9 photo gallery website project",
+    "dateTime": "6-8-2024 1:06 AM"
   },
   {
-    "url": "https://drive.google.com/uc?export=download&id=1G1pAtHkwhz8TmxMvGmEnfXdxejear3m2",
-    "dateTime": "3-8-2024 1:02 AM",
-    "fileName": "Class 8&9 week 7 day 6 practice set pdf "
-  },
-  {
-    "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 8&9 week 7 day 5",
-    "dateTime": "2-8-2024 1:03 AM"
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1GadcWO9RRps7SUpr-z9ZSl30Dt6g6aF6",
-    "dateTime": "2-8-2024 1:02 AM",
-    "fileName": "Class 8&9 week 7 day 5"
+    "type": "file",
+    "url": "https://drive.google.com/uc?export=download&id=1DCwYWwVocl-1tDQGpoZJ4AXNC9riOhka",
+    "dateTime": "6-8-2024 1:05 AM",
+    "fileName": "class 8 & 9 photo gallery website project"
   },
   {
     "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 8&9 week 7 day 4",
-    "dateTime": "1-8-2024 1:01 AM"
+    "message": "class 6& 7 week 8 day 2 Chapter 4 Practice Set.pdf",
+    "dateTime": "6-8-2024 1:04 AM"
   },
   {
-    "url": "https://drive.google.com/uc?export=download&id=1Fl-2HGcyDveipB2onv1zGTsf9I_9wD6x",
-    "dateTime": "1-8-2024 1:00 AM",
-    "fileName": "Class 8&9 week 7 day 4"
-  },
-  {
-    "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 6&7 week 7 day 6",
-    "dateTime": "3-8-2024 1:01 AM"
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1GbUYAgKyJxUhL_CVMAHw9j9ylEy94pqJ",
-    "dateTime": "3-8-2024 1:00 AM",
-    "fileName": "Class 6&7 week 7 day 6"
+    "type": "file",
+    "url": "https://drive.google.com/file/d/1NURLiBIuJ8_nlmfeaMCiUhpojtxJcyMZ/view?usp=drive_link",
+    "dateTime": "6-8-2024 1:02 AM",
+    "fileName": "class 6& 7 week 8 day 2 Chapter 4 Practice Set.pdf"
   },
   {
     "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 6&7 week 7 day 5",
-    "dateTime": "2-8-2024 1:01 AM"
+    "message": "class 6& 7 week 8 day 2",
+    "dateTime": "6-8-2024 1:01 AM"
   },
   {
-    "url": "https://drive.google.com/uc?export=download&id=1Ebj6shEjOH8eTHZCAL-onHK3tqFGuJjA",
-    "dateTime": "2-8-2024 1:00 AM",
-    "fileName": "Class 6&7 week 7 day 5"
-  },
-  {
-    "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 6&7 week 7 day 4",
-    "dateTime": "1-8-2024 1:01 AM"
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1EbCMp3b3QGxmCRtq8Vg4TP4nkkeh0xPw",
-    "dateTime": "1-8-2024 1:00 AM",
-    "fileName": "Class 6&7 week 7 day 4"
+    "type": "file",
+    "url": "https://drive.google.com/uc?export=download&id=1Tcw7PSduTNlO6zGOYCuhn8Tn5ScvgGTK",
+    "dateTime": "6-8-2024 1:00 AM",
+    "fileName": "class 6& 7 week 8 day 2"
   },
   {
     "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 8&9 week 7 day 3 video ",
-    "dateTime": "31-7-2024 1:05 AM"
+    "message": "chat app website project class 8 & 9 ",
+    "dateTime": "5-8-2024 2:03 AM"
   },
   {
-    "url": "https://drive.google.com/uc?export=download&id=1Fj40yN6fRGFtIOn0gWZTWj8WWDr_-7kA",
-    "dateTime": "31-7-2024 1:04 AM",
-    "fileName": "Class 8&9 week 7 day 3 video "
-  },
-  {
-    "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 6&7 week 7 day 3 video , practice design and helping file",
-    "dateTime": "31-7-2024 1:03 AM"
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1EoFXGYJ819ZkSFF_PKPEJYmFjz6PEjRy",
-    "dateTime": "31-7-2024 1:02 AM",
-    "fileName": "Class 6&7 week 7 day 3 helping image"
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1EYHiYmsn_6YBSynRx5WDSsbUwOlg-S14",
-    "dateTime": "31-7-2024 1:01 AM",
-    "fileName": "Class 6&7 week 7 day 3 practice design "
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1EX1YMRCvUqt6S0xEQb-YRvPv8fhhgbJd",
-    "dateTime": "31-7-2024 1:00 AM",
-    "fileName": "Class 6&7 week 7 day 3 video "
+    "type": "file",
+    "url": "https://drive.google.com/file/d/1VTeqTsO--uMT2kjytZ57lrdGTh8QytOh/view?usp=drive_link",
+    "dateTime": "5-8-2024 2:02 AM",
+    "fileName": "chat app website project class 8 & 9 "
   },
   {
     "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 8&9 day 2 video ",
-    "dateTime": "30-7-2024 1:13 AM"
+    "message": "class 6 & 7 week 8 day 1",
+    "dateTime": "5-8-2024 2:01 AM"
   },
   {
-    "url": "https://drive.google.com/uc?export=download&id=1F_IOWvHxQtJx4lHe_diKDIilznS0dcws",
-    "dateTime": "30-7-2024 1:12 AM",
-    "fileName": "Class 8&9 day 2 video "
-  },
-  {
-    "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 6&7 day 2 video ",
-    "dateTime": "29-7-2024 1:11 AM"
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1ETyFeMiNjhE7sRdzz5OSoXg6uWH9Bd2E",
-    "dateTime": "30-7-2024 1:10 AM",
-    "fileName": "Class 6&7 day 2 video "
-  },
-  {
-    "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 8&9 week 7 day 1 video and pdf",
-    "dateTime": "29-7-2024 1:25 AM"
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1G-B4iJwXZ_E7zUrwb8RvB5e7JRT-QzJ9",
-    "dateTime": "29-7-2024 1:24 AM",
-    "fileName": "Class 8&9 week 7 day 1 pdf "
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1FWnUM_-6QMVt4JpO0BxQKPpE8Bal397N",
-    "dateTime": "29-7-2024 1:23 AM",
-    "fileName": "Class 8&9 week 7 day 1 video "
-  },
-  {
-    "type": "text",
-    "url": "",
-    "fileName": "text",
-    "message": "Class 6&7 week 7 day 1 video and pdf",
-    "dateTime": "29-7-2024 1:22 AM"
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1Enj79_oPU69VjzcoaxPN36gO24glJuKC",
-    "dateTime": "29-7-2024 1:21 AM",
-    "fileName": "Class 6&7 week 7 day 1 chapter 4 pdf "
-  },
-  {
-    "url": "https://drive.google.com/uc?export=download&id=1ET-TeX6ujOl9bOYGCENsgjV16bE9jzSx",
-    "dateTime": "29-7-2024 1:20 AM",
-    "fileName": "Class 6&7 week 7 day 1 video "
+    "type": "file",
+    "url": "https://drive.google.com/uc?export=download&id=1FQanvOOLlf6P8cjB3VyPbjoCi64FoCwN",
+    "dateTime": "5-8-2024 2:00 AM",
+    "fileName": "class 6 & 7 week 8 day 1"
   }
-
+]
 
 ];
 module.exports = videoDocuments;
